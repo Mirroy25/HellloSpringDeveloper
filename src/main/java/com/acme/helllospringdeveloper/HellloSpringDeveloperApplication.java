22cc88c31c1a77786b31c1cfeab0c3770a1cc042
RafaelLuyo <u201919295@upc.edu.pe>
2023-08-24 08:19:27 -0500

    chore: initial commit.

---
+++ src/main/java/com/acme/helllospringdeveloper/HellloSpringDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.acme.helllospringdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellloSpringDeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellloSpringDeveloperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/helllospringdeveloper/HellloSpringDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.helllospringdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellloSpringDeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
